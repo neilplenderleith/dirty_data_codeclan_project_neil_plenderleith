d01150075a3888d3ff49f4f0e41961b9614fa701
neilplenderleith <106347514+neilplenderleith@users.noreply.github.com>
2022-07-31 22:17:35 +0100

    Update candy_cleaning_script.R

--- task_4/data_cleaning_scripts/candy_cleaning_script.R
+++ task_4/data_cleaning_scripts/candy_cleaning_script.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 library(here)
+library(janitor)
 
 candy_2015 <- read_excel(here("raw_data/boing-boing-candy-2015.xlsx"))
 candy_2016 <- read_excel(here("raw_data/boing-boing-candy-2016.xlsx"))
@@ -10,3 +11,10 @@ candy_2017 %>%
   count("Q4: COUNTRY")
 
 table(candy_2017["Q4: COUNTRY"])
+names(candy_2015)
+
+candy_2015_clean <- candy_2015 %>% 
+  clean_names()
+
+
+rename_with(~ gsub('[\\[\\]]', '', .x))
