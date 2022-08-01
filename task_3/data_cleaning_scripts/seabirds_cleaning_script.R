5582c5b991ac23ef5990b842fade9c96d0702fc8
neilplenderleith <106347514+neilplenderleith@users.noreply.github.com>
2022-08-01 10:55:22 +0100

    candy cleaning started fml

--- task_3/data_cleaning_scripts/seabirds_cleaning_script.R
+++ task_3/data_cleaning_scripts/seabirds_cleaning_script.R
@@ -25,20 +25,25 @@ seabirds <- left_join(birds, ships, "RECORD ID")
 # I've kept these commented in case anyone running the code didn't 
 # want all the output!
 
+# na_count <- seabirds %>%
+#  summarise(across(.fns = ~ sum(is.na(.x)))) 
+# this counts N/A values across all columns
+
 # Cleaning Data -----------------------------------------------------------
 
 # so from the notes and from the above investigation we can:
 
 # 1 sort column names
-# 2 drop 2 columns with only 1, 4 values out of 49019 (4 more cols were 
+# 2 drop 3 columns with very few values out of 49019 (4 more cols were 
 #   borderline but we will keep these for now)
+# 3 change names of 2 long column names
 
 # lets sort all these uppercase column names
 seabirds <- clean_names(seabirds)
 
-# lets drop cols sex and sal. sex only has 1/49019 values, SAL has 3/49017
+# lets drop cols sex, sal and plphase very few values
 seabirds <- seabirds %>% 
-  select(-sex, -sal)
+  select(-sex, -sal, -plphase)
 
 # lets change some long column names
 seabirds <- seabirds %>% 
--- task_4/data_cleaning_scripts/candy_cleaning_script.R
+++ task_4/data_cleaning_scripts/candy_cleaning_script.R
@@ -10,11 +10,56 @@ candy_2017 <- read_excel(here("raw_data/boing-boing-candy-2017.xlsx"))
 candy_2017 %>% 
   count("Q4: COUNTRY")
 
-table(candy_2017["Q4: COUNTRY"])
+table(candy_2017["Q4: COUNTRY"]) # look at dirty column 'country' eek
 names(candy_2015)
 
+# lets pass through clean_names to get rid of punctuation and bad column names
 candy_2015_clean <- candy_2015 %>% 
   clean_names()
+candy_2016_clean <- candy_2016 %>% 
+  clean_names()
+candy_2017_clean <- candy_2017 %>% 
+  clean_names()
+
+# lets have a look at column names
+names(candy_2015_clean)
+names(candy_2016_clean)
+names(candy_2017_clean)
 
 
-rename_with(~ gsub('[\\[\\]]', '', .x))
+# Order of Operations:
+# Get large pivot data the same for all 3 datasets
+# perform pivot on all three
+# then investigate other columns
+
+# let first start with all the columns between "100_grand_bar" and 
+# "york peppermint butter"
+candy_2015_clean <- candy_2015_clean %>% 
+  #rename("100_grand_bar" = "x100_grand_bar") %>% 
+  relocate(butterfinger, .after = bubble_gum)
+           
+           
+candy_2016_clean <- candy_2016_clean %>% 
+  rename("100_grand_bar" = "x100_grand_bar")
+
+# 2017 data required a q1_, q2_ etc removed from the column names
+candy_2017_clean <- candy_2017_clean %>% 
+  rename_with( ~ str_remove(., pattern = "q[0-90-9]+_"))
+
+
+
+# looks like there is a big pivot longer needed on all data
+# lets have a look at column numbers for each dataset
+
+# 2015 - x100 grand bar to york peppermint patties
+
+candy_2015_long <- candy_2015_clean %>% 
+  pivot_longer(`100_grand_bar`:york_peppermint_patties, names_to = "candy_type", values_to = "candy_rating")
+
+candy_2016_long <- candy_2016_clean %>% 
+  pivot_longer(`100_grand_bar`:york_peppermint_patties, names_to = "candy_type", values_to = "candy_rating")
+
+candy_2017_long <- candy_2017_clean %>% 
+  pivot_longer(`100_grand_bar`:york_peppermint_patties, names_to = "candy_type", values_to = "candy_rating")
+
+
