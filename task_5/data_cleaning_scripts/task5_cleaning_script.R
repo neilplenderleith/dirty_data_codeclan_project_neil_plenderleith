b4923a9e5eca4c8f1085894ecebfd0435b09a0dc
neilplenderleith <106347514+neilplenderleith@users.noreply.github.com>
2022-08-04 14:20:28 +0100

    finally done tasks3+4. task5 done not complete

--- task_5/data_cleaning_scripts/task5_cleaning_script.R
+++ task_5/data_cleaning_scripts/task5_cleaning_script.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readr)
 library(here)
+library(stringr)
 
 rwa <- read_csv("raw_data/rwa.csv")
 
@@ -9,8 +10,6 @@ rwa <- rwa %>%
 rowid_to_column(var = "Id")
 
 
-
-                  
 # Lets alter the reverse scoring
 reverse_scores <- c("Q4", "Q6", "Q8", "Q9", "Q11", "Q13", "Q15", "Q18", "Q20", "Q21")
 
@@ -27,23 +26,27 @@ mutate(across(all_of(reverse_scores), ~ case_when(
   . == 9 ~ 1
 )))
   
-  
-rwa <- rwa %>% 
-  group_by(Id) %>% 
-  mutate(rwa_score = mean(Q3:Q22))
-
- rwa2 %>% 
-  filter(!question %in% c("Q1", "Q2")) %>% 
-  group_by("Id") 
- mutate(rwa_score = mean())
-  
-   
-
-  mutate(rwa_score = mean(where()))
-  
 # secondly we have 4 pivot longers to do
 rwa2 <- rwa %>% 
-  pivot_longer(cols = c(Q1:Q22,E1:E22,TIPI1:TIPI10,VCL1:VCL16), names_to = "question",values_to = "answer")
+  pivot_longer(cols = c(Q1:Q22,E1:E22,TIPI1:TIPI10,VCL1:VCL16), names_to = "question",values_to = "answer")  
+# add on rwa score - could i have made this any more diffcult on myself?
+rwa2 <- rwa2 %>% 
+  filter((!question %in% c("Q1", "Q2")) & (!str_starts(question, "E")) &
+                                          (!str_starts(question, "T")) &
+                                          (!str_starts(question, "V"))) %>%
+  select(Id, question, answer) %>% 
+  group_by(Id) %>% 
+  summarise(rwa_score = mean(answer)) %>% 
+  right_join(rwa2, "Id")
+
+
+rwa2 %>% 
+sum(is.na(gender))
+
+
+
+
+
 
 
 rwa2 %>% 
